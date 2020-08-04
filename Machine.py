6443060a390f219b54c222d5ee86780ea0bd1b61
Apollo13 <calderon.ig08@estudiantec.cr>
2020-08-03 19:30:13 -0600

    Ventana Idioma
    Se inicia la ventana idioma

--- Machine.py
+++ Machine.py
@@ -1,2 +1,45 @@
-from Tkinter import *
+from tkinter import *
+
+# Colores
+
+dark_green = "#718C85"
+brown = "#CBBAA6"
+dark_yellow = "#E8B623"
+purple = "#A4ACC3"
+white = "#FFFFFF"
+
+# fonts
+
+font1 = ("fixedsys", 25)
+font2 = ("fixedsys", 50)
+
+def idioma_screen ():
+
+    # Ventana principal, aqui se encuentra la animacion
+
+    idi_screen = Tk() # se crea una ventana de inicio
+    idi_screen.title ("Advice Machine") # Cambiar el nombre a la ventana
+    idi_screen.geometry ("1200x800+400+100")
+    idi_screen.resizable (0,0)  # No hay cambio de tamaño de la ventana
+
+    # se crea un canvas para dibujar
+
+    canvas_idio = Canvas(idi_screen, width = 1200, height = 800, bg = dark_green)
+    canvas_idio.pack()
+
+    # Botones
+    
+    button_español = Button (idi_screen, text = "Español", font = font1, bg = dark_yellow)
+    button_español.place (x = 200, y = 400, width = 300, height = 100)
+
+    button_ingles = Button(idi_screen, text = "English", font = font1, bg = dark_yellow)
+    button_ingles.place(x = 700, y = 400, width = 300, height = 100)
+
+    # Texto
+    canvas_idio.create_text(600, 100, text = "Idioma / Language", font = font2)
+
+    mainloop()
+
+idioma_screen()
+
 
