dd6a133f7b088d462c522aa157fe789f77a12f87
Apollo13 <calderon.ig08@estudiantec.cr>
2020-08-04 20:33:10 -0600

    cambios en admin

--- Machine.py
+++ Machine.py
@@ -7,12 +7,43 @@ brown = "#CBBAA6"
 dark_yellow = "#E8B623"
 purple = "#A4ACC3"
 white = "#FFFFFF"
-
+gray = "#AEAEAE"
+black = "#000000"
 # fonts
 
 font1 = ("fixedsys", 25)
 font2 = ("fixedsys", 50)
 font3 = ("arial", 18)
+font4 = ("fixedsys", 20)
+
+# Cargar idioma
+
+
+def set_idi (idioma):
+    global glo_idioma, reset_var, off_var, return_var, ex_var
+    global conse_var, dicho_var, chiste_var
+    glo_idioma = idioma
+    if idioma == "español":
+        # Texto machine
+        conse_var = "Consejos"
+        dicho_var = "Dichos"
+        chiste_var = "Chistes"
+        # Texto admin
+        reset_var = "Restaurar"
+        off_var = "Apagar"
+        return_var = "Volver"
+        ex_var = "Generar Excel"
+    elif idioma == "ingles":
+        # Texto machine
+        conse_var = "Advice"
+        dicho_var = "Dichos"
+        chiste_var = "Chistes"
+        # Texto admin
+        reset_var = "Reset"
+        off_var = "Turn off"
+        return_var = "Return"
+        ex_var = "Generate Excel"
+
 
 
 # ---------------------------  Abriri Ventanas  ---------------------------- #
@@ -23,8 +54,9 @@ def openAdmin(window):
     admin()
 
 
-def openMachine(window):
+def openMachine(window, idioma):
     window.destroy()
+    set_idi(idioma)
     machine()
 
 
@@ -54,11 +86,12 @@ def idioma_screen ():
 
     button_español = Button (idi_screen, text = "Español", font = font1, bg = dark_yellow,
                              activebackground = purple, activeforeground = dark_yellow,
-                             command = lambda x = idi_screen: openMachine(x))
+                             command = lambda x = idi_screen, y = "español": openMachine(x, y))
     button_español.place (x = 200, y = 400, width = 300, height = 100)
 
     button_ingles = Button(idi_screen, text = "English", font = font1, bg = dark_yellow,
-                           activebackground = purple, activeforeground = dark_yellow)
+                           activebackground = purple, activeforeground = dark_yellow,
+                             command = lambda x = idi_screen, y = "ingles": openMachine(x, y))
     button_ingles.place(x = 700, y = 400, width = 300, height = 100)
 
     # Texto
@@ -112,6 +145,7 @@ def machine ():
 
 def admin():
     global machine_screen,admin_screen
+    global glo_idioma, reset_var, off_var, return_var, ex_var
     try:
         machine_screen.destroy()
     except:
@@ -127,17 +161,32 @@ def admin():
 
     # se crea un canvas para dibujar
 
-    canvasP = Canvas(admin_screen, width = 1200, height = 800, bg = dark_yellow, highlightbackground = dark_green)
+    canvasP = Canvas(admin_screen, width = 1200, height = 800, bg = gray, highlightbackground = gray)
     canvasP.pack()
 
-    #Botones
-    button = Button (admin_screen, text = "prueba2", font = font1, bg = dark_yellow,
+    #Botones   HACER EL TEXTO VARIABLE
+        # Volver a la maquina
+    button_return = Button (admin_screen, text = return_var, font = font4, bg = gray,
                              activebackground = purple, activeforeground = dark_yellow,
-                     command = lambda x = admin_screen: openMachine(x))
-    button.place (x = 200, y = 400, width = 300, height = 100)
+                     command = lambda x = admin_screen, y = glo_idioma : openMachine(x, y))
+    button_return.place (x = 20, y = 700, width = 350, height = 50)
+        # Resetear la maquina
+    button_reset = Button(admin_screen, text= reset_var, font=font4, bg=gray,
+                           activebackground=purple, activeforeground=dark_yellow)
+    button_reset.place(x=20, y=400, width=350, height=50)
+        # Apagar la maquina
+    button_off = Button(admin_screen, text= off_var, font=font4, bg=gray,
+                           activebackground=purple, activeforeground=dark_yellow)
+    button_off.place(x=20, y=500, width=350, height=50)
+        # Excel
+    button_ex = Button(admin_screen, text= ex_var, font=font4, bg=gray,
+                           activebackground=purple, activeforeground=dark_yellow)
+    button_ex.place(x=20, y=600, width=350, height=50)
+
+    # Dibujos
+    canvasP.create_rectangle(400, 20, 1180, 780, fill = white)
+
     mainloop()
-
-
     
 if __name__ == "__main__" :
     idioma_screen()
